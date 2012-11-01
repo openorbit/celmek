09d7e4501897d6eac694403e157b5cdabb4aa28b
Mattias Holm <lorrden@openorbit.org>
2012-11-01 16:47:06 +0100

    Update copyright headers with contact info

--- src/de4xx.c
+++ src/de4xx.c
@@ -1,5 +1,6 @@
 /*
- * Copyright (c) 2012, Mattias Holm. All rights reserved.
+ * Copyright (c) 2012, Mattias Holm <lorrden(at)openorbit.org>.
+ * All rights reserved.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
